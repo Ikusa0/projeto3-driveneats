591ef4152a7223b3f40f712e20abd1ff4f21e07e
ayrtonporto <22572595+Ikusa0@users.noreply.github.com>
2022-04-01 04:01:57 -0300

    Complete Script Refactor

--- js/app.js
+++ js/app.js
@@ -1,108 +1,107 @@
-const items = document.querySelectorAll(".item-wrap"); // All items for sell
-const button = document.querySelector(".footer").children[0]; // Main button
-// Object to store order's information
-const order = {
-  prato: null,
-  bebida: null,
-  sobremesa: null,
-};
-const background = document.querySelector(".background"); // White background shown in order confirmation dialog
-const orderDialog = document.querySelector(".order"); // Order confirmation dialog
-const whatsapp = "https://wa.me/5583988436116?"; // Restaurant whatsapp address
+const WHATSAPP = "https://wa.me/5583988436116?";
 
-// Add a 'click' event listener to every item
-for (const item of items) {
-  item.addEventListener("click", () => {
-    const parent = item.parentElement;
+// Add a 'onclick' to every item
+for (const item of document.querySelectorAll(".item-wrap")) {
+  item.onclick = () => {
+    const parentId = `#${item.parentElement.id}`;
+    const selected = document.querySelector(`${parentId} .item-selected`);
+    const footerButton = document.querySelector(".footer button");
+
     // Remove other selected items from the same row
-    for (const child of parent.children) {
-      if (child.classList.contains("item-selected")) {
-        child.classList.remove("item-selected");
-        child.children[2].classList.add("display-none");
-      }
+    if (selected) {
+      selected.classList.remove("item-selected");
+      selected.querySelector(".icon").classList.add("display-none");
     }
+
     // Activate the clicked one
     item.classList.add("item-selected");
-    item.children[2].classList.remove("display-none");
-
-    const type = parent.parentElement.id; // Get row name by parent's id
-
-    // Store clicked item's information
-    order[type] = {
-      name: item.children[1].children[0].textContent,
-      description: item.children[1].children[1].textContent,
-      price: Number(item.children[1].children[2].textContent.slice(3).replace(",", ".")),
-    };
+    item.querySelector(".icon").classList.remove("display-none");
 
     // Activate button only when all 3 rows have a selected item
-    if (Object.values(order).every(Boolean)) {
-      button.classList.add("button-active");
-      button.textContent = "Fechar pedido";
-      button.disabled = false;
+    if (document.querySelectorAll(".item-selected").length === 3) {
+      footerButton.classList.add("button-active");
+      footerButton.innerHTML = "Fechar pedido";
+      footerButton.disabled = false;
     }
-  });
+  };
+}
+
+function priceToNumber(price) {
+  return Number(price.replace(",", "."));
+}
+
+function numberToPrice(number) {
+  return `R$ ${number.toFixed(2)}`.replace(".", ",");
 }
 
 // Open order dialog with selected items information
 function openOrderDialog() {
-  orderDialog.children[1].children[0].textContent = order.prato.name;
-  orderDialog.children[1].children[1].textContent = order.prato.price.toFixed(2).replace(".", ",");
+  const background = document.querySelector(".background");
+  const orderDialog = document.querySelector(".order-dialog");
+  const allSelected = document.querySelectorAll(".item-selected");
+  let totalPrice = 0;
 
-  orderDialog.children[2].children[0].textContent = order.bebida.name;
-  orderDialog.children[2].children[1].textContent = order.bebida.price.toFixed(2).replace(".", ",");
+  for (const selected of allSelected) {
+    const parentId = `#${selected.parentElement.id}`;
+    const foodName = orderDialog.querySelector(`${parentId} span`);
+    const foodPrice = orderDialog.querySelector(`${parentId} span:last-child`);
 
-  orderDialog.children[3].children[0].textContent = order.sobremesa.name;
-  orderDialog.children[3].children[1].textContent = order.sobremesa.price.toFixed(2).replace(".", ",");
+    foodName.innerHTML = selected.querySelector("h4").innerHTML;
+    foodPrice.innerHTML = selected.querySelector("h5").innerHTML.slice(3);
 
-  orderDialog.children[4].children[1].textContent = `R$ ${(
-    order.prato.price +
-    order.bebida.price +
-    order.sobremesa.price
-  )
-    .toFixed(2)
-    .replace(".", ",")}`;
+    totalPrice += priceToNumber(foodPrice.innerHTML);
+  }
+
+  orderDialog.querySelector("#total span:last-child").innerHTML = numberToPrice(totalPrice);
 
   background.classList.remove("display-none");
   orderDialog.classList.remove("display-none");
 }
 
 function cancelOrder() {
+  const background = document.querySelector(".background");
+  const orderDialog = document.querySelector(".order-dialog");
+
   background.classList.add("display-none");
   orderDialog.classList.add("display-none");
 }
 
 function resetOrder() {
+  const allSelected = document.querySelectorAll(".item-selected");
+  const footerButton = document.querySelector(".footer button");
+
   cancelOrder();
 
-  order.prato = null;
-  order.bebida = null;
-  order.sobremesa = null;
-
-  for (const item of document.querySelectorAll(".item-selected")) {
-    item.classList.remove("item-selected");
-    item.children[2].classList.add("display-none");
+  for (const selected of allSelected) {
+    selected.classList.remove("item-selected");
+    selected.querySelector(".icon").classList.add("display-none");
   }
 
-  button.classList.remove("button-active");
-  button.textContent = "Selecione os 3 itens para fechar seu pedido";
-  button.disabled = true;
+  footerButton.classList.remove("button-active");
+  footerButton.textContent = "Selecione os 3 itens para fechar seu pedido";
+  footerButton.disabled = true;
 }
 
 // Send a whatsapp message with order's information
 function makeOrder() {
+  const completeOrder = document.querySelectorAll(".order-dialog span:first-child");
+  const orderPrice = document.querySelector(".order-dialog #total span:last-child").innerHTML;
+
   let name = prompt("Antes de prosseguir, por favor informe seu nome: ");
   if (!name) name = "não informado.";
+
   let address = prompt("Por favor, informe também o endereço: ");
   if (!address) address = "não informado.";
+
   const message = encodeURIComponent(`Olá, gostaria de fazer o pedido:
-- Prato: ${order.prato.name}
-- Bebida: ${order.bebida.name}
-- Sobremesa: ${order.sobremesa.name}
-Total: R$ ${(order.prato.price + order.bebida.price + order.sobremesa.price).toFixed(2).replace(".", ",")}
+- Prato: ${completeOrder[0].innerHTML}
+- Bebida: ${completeOrder[1].innerHTML}
+- Sobremesa: ${completeOrder[2].innerHTML}
+Total: ${orderPrice}
     
 Nome: ${name}
 Endereço: ${address}`);
 
-  window.open(whatsapp + "text=" + message);
+  window.open(WHATSAPP + "text=" + message);
   resetOrder();
 }
